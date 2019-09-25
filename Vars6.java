7d2b5cd3ba48c1c705d6196cf819232fa5c6b0bd
IzanMC <55711575+IzanMC@users.noreply.github.com>
2019-09-25 18:21:41 +0200

    Delete Vars6.java

--- Vars6.java
+++
@@ -1,9 +0,0 @@
-public class Vars6{
-    public static void main(String[] args) {
-        char lletra1 = 'h';
-        char lletra2 = 'o';
-        char lletra3 = 'l';
-        char lletra4 = 'a';
-        System.out.println(""+lletra1+lletra2+lletra3+lletra4);
-    }
-}
